e0383fde285502047fd3d5caccd37c64e26e4047
Pedro Fontes <pedro7fontes@gmail.com>
2025-03-19 15:40:57 -0300

    add sandbox url

--- src/routes/plans.ts
+++ src/routes/plans.ts
@@ -10,7 +10,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-10-16",
 });
 const asaas = new AsaasClient(process.env.ASAAS_API_KEY!, {
-  sandbox: false,
+  sandbox: true,
 });
 
 // Rota para listar planos disponíveis
